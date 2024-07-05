438f88aebf85827d78223f4446d8f30d82c2fbd2
gouky10 <hansromero7@gmail.com>
2024-07-05 12:37:36 -0500

    Create database models schema

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class Department(Base):
+    __tablename__ = 'departments'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+class Job(Base):
+    __tablename__ = 'jobs'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+
+class Employee(Base):
+    __tablename__ = 'employees'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    datetime = Column(DateTime)
+    Department_id = Column(Integer, ForeignKey('departments.id'))
+    job_id = Column(Integer, ForeignKey('jobs.id'))
+
+    department = relationship('Department')
+    job = relationship('Job')
